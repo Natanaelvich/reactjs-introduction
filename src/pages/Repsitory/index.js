083db5849e8bbbe8b69b1e5213025be952d034f6
Natanael Lima <taelima1997@gmail.com>
2020-01-30 00:14:19 -0300

    finished all sytles

--- src/pages/Repsitory/index.js
+++ src/pages/Repsitory/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IsssueList } from './styles';
 
 export default class Repsitory extends Component {
   constructor(props) {
@@ -52,11 +53,34 @@ export default class Repsitory extends Component {
         ) : (
           <Container>
             <Owner>
+              <Link to="/">Voltar as repositorios</Link>
               <img src={owner.avatar_url} alt={owner.login} />
               <h1>{owner.login}</h1>
               <h1>{repos.name}</h1>
               <p>{repos.description}</p>
             </Owner>
+
+            <IsssueList>
+              {issuesData.map(issue => (
+                <li key={String(issue.id)}>
+                  <img
+                    src={issue.user.avatar_url}
+                    alt={issue.user.login}
+                  />
+                  <div>
+                    <strong>
+                      <a href={issue.html_url}>{issue.title}</a>
+                      {issue.labels.map(label => (
+                        <span key={String(label.id)}>
+                          {label.name}
+                        </span>
+                      ))}
+                    </strong>
+                    <p>{issue.user.login}</p>
+                  </div>
+                </li>
+              ))}
+            </IsssueList>
           </Container>
         )}
       </div>
